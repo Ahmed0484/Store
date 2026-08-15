811889d8fd8c8e4ec5d85e74763451f4ddfb0f90
Ahmed0484 <a_elshazly_08@hotmail.com>
2026-08-15 15:57:33 +0300

    api basics

---
+++ API/Program.cs
@@ -0,0 +1,17 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(x =>
+{
+    x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+app.MapControllers();
+DbInitializer.InitDb(app);
+app.Run();
