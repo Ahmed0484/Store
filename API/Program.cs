0c2546f29ba63ea360dfad05c1bba9d006aba66e
Ahmed0484 <a_elshazly_08@hotmail.com>
2026-09-03 16:04:36 +0300

    shopping cart

--- API/Program.cs
+++ API/Program.cs
@@ -17,9 +17,12 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
-app.UseCors(opt => 
+app.UseCors(opt =>
 {
-    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
+    opt.AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+    .WithOrigins("https://localhost:3000");
 });
 app.MapControllers();
 DbInitializer.InitDb(app);
--- client/src/app/api/baseApi.ts
+++ client/src/app/api/baseApi.ts
@@ -1,41 +1,51 @@
-import { fetchBaseQuery, type BaseQueryApi, type FetchArgs } from "@reduxjs/toolkit/query/react";
+import { fetchBaseQuery, type BaseQueryApi, type FetchArgs } from "@reduxjs/toolkit/query";
 import { startLoading, stopLoading } from "../layout/uiSlice";
 import { toast } from "react-toastify";
 import { router } from "../routes/Routes";
 
 const customBaseQuery = fetchBaseQuery({
-    baseUrl: 'https://localhost:5001/api'
+    baseUrl: 'https://localhost:5001/api',
+    credentials:'include'
 });
 
-const sleep = () => new Promise(resolve => setTimeout(resolve, 1000));
+type ErrorResponse = | string | {title: string} | {errors: string[]};
 
-export const baseQueryWithErrorHandling = async (args: string | FetchArgs, api: BaseQueryApi,
+const sleep = () => new Promise(resolve => setTimeout(resolve, 200));
+
+export const baseQueryWithErrorHandling = async (args: string | FetchArgs, api: BaseQueryApi, 
     extraOptions: object) => {
     api.dispatch(startLoading());
     await sleep();
     const result = await customBaseQuery(args, api, extraOptions);
     api.dispatch(stopLoading());
     if (result.error) {
-        const { status, data } = result.error;
-        switch (status) {
+        console.log(result.error);
+
+        const originalStatus = result.error.status === 'PARSING_ERROR' && result.error.originalStatus
+            ? result.error.originalStatus
+            : result.error.status
+
+        const responseData = result.error.data as ErrorResponse;
+
+        switch (originalStatus) {
             case 400:
-                if (typeof data === "string")
-                    toast.error(data);
-                else if (typeof data === 'object' && !('errors' in (data as MultiErrors)))
-                    toast.error((data as OneError).title);
-                else if (typeof data === 'object' && 'errors' in (data as MultiErrors))
-                    throw Object.values((data as MultiErrors).errors).flat().join(', ')
+                if (typeof responseData === 'string') toast.error(responseData);
+                else if ('errors' in responseData) {
+                    throw Object.values(responseData.errors).flat().join(', ')
+                }
+                else toast.error(responseData.title);
                 break;
             case 401:
-                toast.error((data as OneError).title);
+                if (typeof responseData === 'object' && 'title' in responseData)
+                    toast.error(responseData.title);
                 break;
             case 404:
-                if (typeof data === 'object' && 'title' in (data as OneError))
+                if (typeof responseData === 'object' && 'title' in responseData)
                     router.navigate('/not-found')
                 break;
             case 500:
-                if (typeof data === 'object')
-                    router.navigate('/server-error', {state: {error: data}})
+                if (typeof responseData === 'object')
+                    router.navigate('/server-error', {state: {error: responseData}})
                 break;
             default:
                 break;
@@ -44,6 +54,3 @@ export const baseQueryWithErrorHandling = async (args: string | FetchArgs, api: 
 
     return result;
 }
-
-type OneError = { title: string }
-type MultiErrors = { errors: string[] }
